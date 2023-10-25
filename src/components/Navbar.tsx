5142868d585a702a94db40e42ab8ef19c9676af4
Felipe França <felipefl200@gmail.com>
2023-10-25 13:55:41 -0300

    Change navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import clasnames from "classnames";
 
 const links = [
   { label: "Dashboard", href: "/" },
@@ -7,6 +10,7 @@ const links = [
 ];
 
 export function Navbar() {
+  const currentPath = usePathname();
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 items-center h-14 shadow">
       <Link
@@ -19,7 +23,12 @@ export function Navbar() {
         {links.map((link) => (
           <li
             key={link.href}
-            className="text-zinc-600 hover:text-zinc-800 relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-zinc-800 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center"
+            className={clasnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-600": link.href !== currentPath,
+              "hover:text-zinc-800 relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-zinc-800 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center":
+                true,
+            })}
           >
             <Link href={link.href}>{link.label}</Link>
           </li>
